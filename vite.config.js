78cc8d85112993e43449b5a009c2a30fc7fe1902
VCoklat <VCoklat@users.noreply.github.com>
2026-09-09 11:16:25 +0000

    Implement structural updates and optimizations across multiple modules

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 3000,
     // Proxy request /api ke server FastAPI lokal
     proxy: {
